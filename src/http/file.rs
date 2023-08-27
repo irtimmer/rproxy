ed2e73ce55b06a14d4352ca8b1a0f8653e2c0fc0
Iwan Timmer <irtimmer@gmail.com>
2023-08-27 14:47:13 +0200

    http: add guessed mime type for files

--- src/http/file.rs
+++ src/http/file.rs
@@ -7,6 +7,7 @@ use futures::StreamExt;
 use http_body_util::{combinators::BoxBody, StreamBody};
 
 use hyper::body::{Bytes, Frame, Incoming};
+use hyper::header::{CONTENT_LENGTH, CONTENT_TYPE};
 use hyper::{Request, Response};
 
 use tokio::fs::File;
@@ -31,11 +32,16 @@ impl HttpService for FileService {
         // UNSAFE: path is not validated
         let path = self.path.join(&req.uri().path()[1..]);
         let file = File::open(path).await?;
+        let meta = file.metadata().await?;
         let stream = ReaderStream::new(file);
         let body = StreamBody::new(
             stream.map(|s| s.map(|b| Frame::data(b)).map_err(|e| Box::new(e) as Error)),
         );
-        let bbody = BoxBody::new(body);
-        Ok(Response::builder().body(bbody)?)
+        let mut builder = Response::builder().header(CONTENT_LENGTH, meta.len());
+        if let Some(mime) = mime_guess::from_path(req.uri().path()).first_raw() {
+            builder = builder.header(CONTENT_TYPE, mime);
+        }
+
+        Ok(builder.body(BoxBody::new(body))?)
     }
 }
